d5da97f76ea1deff126ebf530170326eb0d45b47
Deepak22-Devs <jagannath.behera1970dec@gmail.com>
2026-08-12 16:48:31 +0530

    Updated 12.08.26

--- Chapter11/06_property_decoraters.py
+++ Chapter11/06_property_decoraters.py
@@ -1,9 +1,23 @@
 class Employee:
     a = 1
-    @classmethod   #it priotirise to class atrributes instaed of instance attributes
+    @classmethod  
     def show(cls):
         print(f"The class attribute of a is {cls.a}")
 
+    @property
+    def name(self):
+        return f"{self.fname} {self.mname} {self.lname}"
+
+    @name.setter
+    def name(self,value):
+        self.fname = value.split(" ")[0]
+        self.mname = value.split(" ")[1]
+        self.lname = value.split(" ")[2]
+
 e = Employee()
 e.a = 45
+
+e.name = "Deepak Kumar Behera"
+print(e.fname ,e.mname, e.lname)
+print(e.name)
 e.show()
